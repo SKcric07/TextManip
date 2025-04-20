16832d9013adc13b4067d7de103ba461da6ddfef
SKcric07 <p.sanjaykriplani@gmail.com>
2025-04-20 16:13:22 -0700

    Created the file structure for the Main Body of our app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar/navbar';
+import TextForm from './Components/TextForm/textform';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <TextForm></TextForm>
     </div>
   );
 }
---
+++ src/Components/TextForm/textform.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TextForm.css'
+function TextForm() {
+  return (
+    // This will contain the Main Body and they Text field
+    <div>
+      <p>Text Input</p>
+    </div>
+  )
+}
+
+export default TextForm
