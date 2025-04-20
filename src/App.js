1d3c6996a6810a76af55159c0f4dc95a9c9dfaa1
SKcric07 <p.sanjaykriplani@gmail.com>
2025-04-20 14:43:35 -0700

    Basic skeleton is ready for the navbar just need to beautify it.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Navbar from './Components/Navbar/navbar';
-import TextForm from './Components/TextForm/textform';
 
 function App() {
   return (
--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -4,7 +4,49 @@ import './navbar.css';
 function Navbar() {
   return (
     <div>
-      <h2>This is Head</h2>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <div className='navbar-left'> {/* Contains all the Logo Navbar links */}
+            {/* Brand text */}
+            <div className='navbar-brand'>
+              <span className='Brand-text'>TextManip</span>
+            </div>
+
+            {/* Nagivation Links */}
+            <div className='navbar-links'>
+              <a href="/" className='nav-link'>
+                Home
+              </a>
+              <a href="/" className='nav-link'>
+                About
+              </a>
+              <a href="/" className='nav-link'>
+                Contact Us
+              </a>
+            </div>
+          </div>
+
+          {/* Right-side: User logo, User name, Logout button, Dark mode Toggle */}
+          <div className='navbar-right'>
+            <div className='user-info'>
+              <a href="/user-profile" className='user-link'>
+                <div className='user-avatar'>
+                  <span className='user-icon'>
+                  <i className="fa-solid fa-circle-user"></i>
+                  </span>
+                </div>
+                <span className='username'>Sanjay Kriplani</span>
+              </a>
+            </div>
+
+            {/* <button className='dark-mode-toggle'>
+
+            </button> */}
+
+            <button className='logout-button'>Logout</button>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
