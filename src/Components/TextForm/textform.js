72876cb968e9139b7879ef00a7d3c5648ca6b0ef
SKcric07 <p.sanjaykriplani@gmail.com>
2025-04-22 23:26:49 -0700

    Added text statistics word count and character count in textform

--- src/Components/TextForm/textform.js
+++ src/Components/TextForm/textform.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './TextForm.css'
+
 function TextForm() {
   return (
     // This will contain the Main Body and they Text field
-    <div>
+    <div className='text-form-container'>
       <div className="form-section">
         <h2 className='section-title'>Text Input</h2>
         <textarea 
@@ -12,10 +13,16 @@ function TextForm() {
 
         </textarea>
 
+        <div className="text-stats">
+          <span>10 words</span>
+          <span>10 characters</span>
+        </div>
+
         <div className="action-buttons">
           <button className="action-button">UPPERCASE</button>
           <button className="action-button">lowercase</button>
           <button className="action-button">Capitalize</button>
+          <button className="action-button">Remove Space</button>
           <button className="action-button clear">Clear</button>
         </div>
       </div>
